1f86c6e4be4f16ec7358f39918b0c3553e8852ba
Anthony Bui <anthonybui@gmail.com>
2017-02-22 20:35:57 -0500

    More configuration

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,24 @@ module.exports = {
         test: /\.js$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              }
+            }
+          }
+        ]
       }
     ]
   },
