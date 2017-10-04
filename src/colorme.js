10e77087a796ed8aad45b5506473c2103541f108
Anthony Bui <anthonybui@gmail.com>
2017-10-04 09:31:49 -0400

    COLOR ME

--- src/colorme.js
+++ src/colorme.js
@@ -1,7 +1,6 @@
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
-import 'p5.serialserver/lib/p5.serialport';
 import _ from 'lodash';
 
 const sketch = (p5) => {
@@ -9,11 +8,55 @@ const sketch = (p5) => {
 
   let points = [];
   var colors = [];
-  var serial;
+  var color_meta = [
+    {
+      name: "LightGoldenRodYellow",
+      hex: "#FAFAD2",
+      description: "Unique among accepted HTML color names, LightGoldenRodYellow is the longest."
+    }, {
+      name: "COFFEE",
+      hex: "#C0FFEE",
+      description: "A cute color with a cute hex code for the one and only love of our lives: coffee."
+    },
+    {
+      name: "Coffee-pink",
+      hex: "#FFCCC0",
+      description: "The complimentary color to coffee."
+    },
+    {
+      name: "Machine-Learned Red",
+      hex: "#D95E50",
+      description: "Machine-learned red..."
+    },
+    {
+      name: "Eigengrau",
+      hex: "#16161D",
+      description: "A dark gray that literally translates to \"intrinsic gray\" or \"own gray\", " +
+        "Eigengrau is the dark gray the human eye sees in absolute darkness. " +
+        "\n\nEigengrau is a phenonomenon believed to be caused by intrinsic noise in our optic nerves. " +
+        "As a result, it is often referred to as \"visual noise\"."
+    },
+    {
+      name: "Metallic Gold",
+      hex: "#D4AF37",
+      description: "A simple metallic gold, slightly darker and seen in leafing. When used in tiny hints, reveals fun and festivity."
+    }
+      // nixie orange???
+  ]
+
+  var goldimg = require('./assets/gold-leaf.jpg');
+  var img;
+  var active = null;
+  var width;
+
+
 
   p5.setup = () => {
-    // serial = new p5.SerialPort();
+    width = p5.windowWidth / 6;
+
     let canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+
+
     // function getRandomPoint() {
     //   return {
     //     x: random(p5.windowWidth),
@@ -29,14 +72,13 @@ const sketch = (p5) => {
     // colors.push(p5.color("#D4AF37")); // metallic gold
 
     colors.push(p5.color("#FAFAD2")); // lightgoldenrodyellow
-    colors.push(p5.color("#FFC0D0")); // light accent
     colors.push(p5.color("#c0ffee")); // main brand
+    colors.push(p5.color("#FFCCC0")); // light accent
     colors.push(p5.color("#D95E50")); // dark accent
     colors.push(p5.color("#16161D")); // dark shades
     colors.push(p5.color("#D4AF37")); // metallic gold
 
     // colors.push(p5.color("#A89447")); // gold leaf
-
     // colors.push(p5.color('cyan'));
     // colors.push(p5.color("#09ffc3")); // slurp
 
@@ -48,6 +90,7 @@ const sketch = (p5) => {
 
     // colors.push(p5.color("#FA5C1A"));
 
+    img = p5.loadImage(goldimg);
 
 
      _.times(5, function() {
@@ -56,17 +99,67 @@ const sketch = (p5) => {
   }
 
   p5.draw = () => {
-    p5.background(p5.color("yellow"), 85);
+    function display_meta() {
+      p5.fill(255);
+      p5.textFont("Roboto");
+      p5.textSize(48);
+      p5.textStyle("BOLD");
+      p5.text(color_meta[active]["name"], 200, 200);
 
-    var width = p5.windowWidth / 6;
+      p5.fill(255);
+      p5.textFont("Roboto");
+      p5.textSize(20);
+      p5.textStyle("NORMAL");
+      p5.text(color_meta[active]["description"], 200, 290, 450, p5.windowHeight);
+
+      p5.fill(255);
+      p5.textFont("Inconsolata");
+      p5.textSize(32);
+      p5.text(color_meta[active]["hex"], 200, 250);
+    }
+
+    p5.background(p5.color("white"), 85);
 
     p5.noStroke();
 
-    for (var i = 0; i < 6; i++) {
-      p5.fill(colors[i]);
-      p5.rect(width * i, 0, width, p5.windowHeight);
+    if (active != null && active != 5) {
+      p5.background(colors[active]);
+      //console.log(color_meta[active]["name"]);
+      display_meta();
+    } else if (active == 5) {
+      p5.image(img, 0, 0, p5.windowWidth, p5.windowHeight);
+      display_meta();
+    } else {
+      for (var i = 0; i < 6; i++) {
+        p5.fill(colors[i]);
+
+        if (p5.mouseX > width * i && p5.mouseX < width * (i+1)) {
+          p5.rect(width * i, 0, width, p5.windowHeight);
+        } else {
+          p5.rect(width * i, 0.1 * p5.windowHeight, width, 0.8 * p5.windowHeight);
+        }
+      }
+
+      // gold!
+      if (p5.mouseX > width * 5 && p5.mouseX < width * 6) {
+        p5.image(img, p5.windowWidth - width, 0, width, p5.windowHeight, 0, 0, width, p5.windowHeight);
+      } else {
+        p5.image(img, p5.windowWidth - width, 0.1 * p5.windowHeight, width, 0.8 * p5.windowHeight, 0, 0.1 * p5.windowHeight, width, 0.8 * p5.windowHeight);
+      }
     }
   }
+
+  p5.mouseClicked = () => {
+    if (active == null) {
+      active = _.floor(p5.mouseX / width);
+    } else {
+      active = null;
+    }
+  }
+
+//  p5.keyPressed = () => {
+//    active = null;
+//  }
 }
 
 // MEDIUMAQUAMARINE
